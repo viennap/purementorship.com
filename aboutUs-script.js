c40cf3ee3cf1d35c8836198260b351e3f0102465
Arav <arav.ravula@gmail.com>
2023-08-02 16:59:51 -0400

    New about-us with lorrens mark ups

--- aboutUs-script.js
+++ aboutUs-script.js
@@ -3,6 +3,20 @@ const closeButton = document.getElementById('close-button')
 const popup = document.getElementById('popup1')
 const overlay = document.getElementById('overlay')
 
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+
 openButton.addEventListener('click', () =>{
     popup.classList.add('active')
     overlay.classList.add('active')
